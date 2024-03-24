c6f927764fd5eb4291f7e5bc7d38da4ea856848f
Muhammad Daffa' A.R <daffavcd@gmail.com>
2024-03-24 23:11:57 +0700

    dashboard, list, insert

--- src/views/bucket/AddBucket.js
+++ src/views/bucket/AddBucket.js
@@ -1,29 +1,111 @@
-import React from 'react'
+import { useEffect, useState, React } from 'react'
 import {
   CCard,
   CCardBody,
   CCardHeader,
   CCol,
   CRow,
-  CTable,
-  CTableBody,
   CButton,
-  CTableDataCell,
-  CTableHead,
-  CTableHeaderCell,
-  CTableRow,
   CNavLink,
   CForm,
   CFormInput,
   CFormText,
+  CFormTextarea,
 } from '@coreui/react'
 import { NavLink } from 'react-router-dom'
 
 import CIcon from '@coreui/icons-react'
 
-import { cilSave, cilMagnifyingGlass, cilPencil, cilTrash, cilList } from '@coreui/icons'
+import { cilSave, cilList } from '@coreui/icons'
+
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 const Tables = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    location: '',
+    estimated_budget: '',
+    description: '',
+    authorId: 1,
+  })
+
+  const [image, setImage] = useState(null)
+
+  const handleChange = (e) => {
+    const { id, value } = e.target
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      // UPLOAD IMAGE
+      // const formData = new FormData()
+      // formData.append('image', image)
+
+      // const responseImage = await fetch('/public/images/travels', {
+      //   method: 'POST',
+      //   body: formData,
+      // })
+
+      // if (!responseImage.ok) {
+      //   throw new Error('Failed to upload image')
+      // }
+
+      // console.log('Image uploaded successfully')
+
+      const response = await fetch(`${process.env.SATYR_SERVER}/travels`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      })
+      if (!response.ok) {
+        throw new Error(`HTTP error: Status ${response.status}`)
+      }
+      console.log('Data posted successfully')
+      setFormData({
+        name: '',
+        location: '',
+        estimated_budget: '',
+        description: '',
+        image: '',
+        authorId: 1,
+      })
+
+      withReactContent(Swal)
+        .fire({
+          title: 'Success',
+          text: 'The item was inserted',
+          confirmButtonText: 'Oke',
+        })
+        .then((result) => {
+          window.location.href = '/#/bucket'
+        })
+    } catch (error) {
+      console.error('Error posting data:', error)
+    }
+  }
+
+  const [imagePreview, setImagePreview] = useState(null)
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0]
+    if (file) {
+      setImage(file)
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        setImagePreview(reader.result)
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+
   return (
     <CRow>
       <CCol xs={12} className="mb-4">
@@ -41,50 +123,73 @@ const Tables = () => {
           </CCardHeader>
           <CCardBody className="p-4">
             <CRow>
-              <CForm>
+              <CForm id="input-form" onSubmit={handleSubmit}>
                 <CCol>
                   <CFormInput
                     type="text"
+                    id="name"
                     label="Destination Name"
                     placeholder="Wisata Ramayana"
                     className="mb-2"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
                   />
                 </CCol>
                 <CCol>
                   <CFormInput
                     type="text"
+                    id="location"
                     label="Location Name"
                     placeholder="Kota Malang"
                     className="mb-2"
+                    value={formData.location}
+                    onChange={handleChange}
+                    required
                   />
                 </CCol>
                 <CCol>
                   <CFormInput
                     type="text"
+                    id="estimated_budget"
                     label="Estimated Budget"
                     placeholder="5000000"
                     className="mb-2"
+                    value={formData.estimated_budget}
+                    onChange={handleChange}
+                    required
                   />
                 </CCol>
                 <CCol>
-                  <CFormText
-                    type="text"
+                  <CFormTextarea
+                    id="description"
                     label="Description"
                     placeholder="Tell them about what you'd recomend abaout this place..."
-                    className="mb-2"
-                  />
+                    rows={3}
+                    value={formData.description}
+                    onChange={handleChange}
+                    required
+                  ></CFormTextarea>
                 </CCol>
                 <CCol>
                   <CFormInput
-                    type="text"
-                    label="Destination Link"
-                    placeholder="Location Link"
-                    aria-describedby="https://www.google.com/maps/search/?api=1&query=47.5951518%2C-122.3316393&query_place_id=ChIJKxjxuaNqkFQR3CK6O1HNNqY"
+                    type="file"
+                    id="image"
+                    label="Destination Image"
                     className="mb-2"
+                    onChange={handleImageChange}
+                    required
                   />
-                </CCol>
-                <CCol>
-                  <CFormInput type="file" label="Destination Image" className="mb-2" />
+                  {imagePreview && (
+                    <img
+                      src={imagePreview}
+                      height={300}
+                      width={240}
+                      className="mt-2"
+                      style={{ objectFit: 'cover' }}
+                      alt="Preview Image"
+                    />
+                  )}
                 </CCol>
                 <CCol style={{ textAlign: 'right' }}>
                   <CButton color="primary" size="lg" type="submit" className="mt-4">
--- src/views/bucket/Bucket.js
+++ src/views/bucket/Bucket.js
@@ -20,6 +20,9 @@ import CIcon from '@coreui/icons-react'
 
 import { cilPlus, cilMagnifyingGlass, cilPencil, cilTrash } from '@coreui/icons'
 
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
 const Buckets = () => {
   const [travels, setTravels] = useState(null)
   useEffect(() => {
@@ -43,68 +46,127 @@ const Buckets = () => {
 
     fetchTravels()
   }, [])
+  const deleteTravel = (key) => {
+    withReactContent(Swal)
+      .fire({
+        title: 'Are you sure?',
+        text: 'The deleted item is irreversible',
+        confirmButtonColor: 'red',
+        confirmButtonText: 'Yes, delete it!',
+        showCancelButton: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          const deleteTravels = async () => {
+            try {
+              const responseDelete = await fetch(`${process.env.SATYR_SERVER}/travels/${key}`, {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+              })
+              console.log(responseDelete)
+              if (!responseDelete.ok) {
+                throw new Error(`HTTP error: Status ${response.status}`)
+              }
+              let responseDeleteJson = await responseDelete.json()
+              console.log(responseDeleteJson)
+
+              // GET TRAVELS
+              const response = await fetch(`${process.env.SATYR_SERVER}/travels`, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+              })
+              console.log(response)
+              if (!response.ok) {
+                throw new Error(`HTTP error: Status ${response.status}`)
+              }
+              let responseJson = await response.json()
+              console.log(responseJson)
+              setTravels(responseJson)
+              Swal.fire({
+                title: 'Deleted!',
+                text: 'Your item has been deleted.',
+                icon: 'success',
+              })
+            } catch (err) {
+              console.log(err)
+            }
+          }
+          deleteTravels()
+        }
+      })
+  }
 
   return (
-    <CRow>
-      <CCol xs={12} className="mb-4">
-        <CNavLink to="/add_bucket" as={NavLink}>
-          <CButton color="primary" className="r-0">
-            <CIcon icon={cilPlus} className="me-2" />
-            Add Travel Bucket
-          </CButton>
-        </CNavLink>
-      </CCol>
-      <CCol xs={12}>
-        <CCard className="mb-4">
-          <CCardHeader className="d-flex">
-            <strong>Travel Bucket List</strong>
-          </CCardHeader>
-          <CCardBody>
-            <p className="text-body-secondary small">All data are displayed in the table bellow</p>
-            <CTable>
-              <CTableHead>
-                <CTableRow>
-                  <CTableHeaderCell scope="col">#</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Name</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Location</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Action</CTableHeaderCell>
-                </CTableRow>
-              </CTableHead>
-              {travels ? (
-                <CTableBody>
-                  {travels.map((travel, index) => (
-                    <CTableRow key={index}>
-                      <CTableHeaderCell scope="row">1</CTableHeaderCell>
-                      <CTableDataCell>{travel.name}</CTableDataCell>
-                      <CTableDataCell>{travel.location}</CTableDataCell>
-                      <CTableDataCell>
-                        <CButton color="info" className="me-2">
-                          <CIcon icon={cilMagnifyingGlass} />
-                        </CButton>
-                        <CButton color="warning" className="me-2">
-                          <CIcon icon={cilPencil} />
-                        </CButton>
-                        <CButton color="danger" className="me-2">
-                          <CIcon icon={cilTrash} />
-                        </CButton>
+    <>
+      <CRow>
+        <CCol xs={12} className="mb-4">
+          <CNavLink to="/add_bucket" as={NavLink}>
+            <CButton color="primary" className="r-0">
+              <CIcon icon={cilPlus} className="me-2" />
+              Add Travel Bucket
+            </CButton>
+          </CNavLink>
+        </CCol>
+        <CCol xs={12}>
+          <CCard className="mb-4">
+            <CCardHeader className="d-flex">
+              <strong>Travel Bucket List</strong>
+            </CCardHeader>
+            <CCardBody>
+              <p className="text-body-secondary small">
+                All data are displayed in the table bellow
+              </p>
+              <CTable>
+                <CTableHead>
+                  <CTableRow>
+                    <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Name</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Location</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Action</CTableHeaderCell>
+                  </CTableRow>
+                </CTableHead>
+                {travels && travels.length > 0 ? (
+                  <CTableBody>
+                    {travels.map((travel, index) => (
+                      <CTableRow key={index}>
+                        <CTableHeaderCell scope="row">{index + 1}</CTableHeaderCell>
+                        <CTableDataCell>{travel.name}</CTableDataCell>
+                        <CTableDataCell>{travel.location}</CTableDataCell>
+                        <CTableDataCell>
+                          <CButton color="info" className="me-2">
+                            <CIcon icon={cilMagnifyingGlass} />
+                          </CButton>
+                          <CButton color="warning" className="me-2">
+                            <CIcon icon={cilPencil} />
+                          </CButton>
+                          <CButton
+                            color="danger"
+                            onClick={() => {
+                              deleteTravel(travel.id)
+                            }}
+                            className="me-2"
+                          >
+                            <CIcon icon={cilTrash} />
+                          </CButton>
+                        </CTableDataCell>
+                      </CTableRow>
+                    ))}
+                  </CTableBody>
+                ) : (
+                  <CTableBody>
+                    <CTableRow>
+                      <CTableDataCell colSpan="4" className="text-center">
+                        No data available.
                       </CTableDataCell>
                     </CTableRow>
-                  ))}
-                </CTableBody>
-              ) : (
-                <CTableBody>
-                  <CTableRow>
-                    <CTableDataCell colspan="4" className="text-center">
-                      No data available.
-                    </CTableDataCell>
-                  </CTableRow>
-                </CTableBody>
-              )}
-            </CTable>
-          </CCardBody>
-        </CCard>
-      </CCol>
-    </CRow>
+                  </CTableBody>
+                )}
+              </CTable>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </>
   )
 }
 
